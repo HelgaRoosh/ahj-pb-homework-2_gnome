e9e099e72fd2f4b0e698d3795a5e1f3d0380ccb0
HelgaRoosh <vulfina@mail.ru>
2022-10-04 11:32:47 +0500

    gnome не решена проблема повтора ячеек при генерации гнома

---
+++ src/js/game_manager.js
@@ -0,0 +1,44 @@
+document.body.style.overflow = 'hidden';// на случай изменение количества по высоте
+window.addEventListener('load', startGame);
+
+const amountLines = 4; // можно менять, но не решена проблема высоты элементов,
+const amountСolumns = 4; // они могут не влезть в окно
+
+function startGame() {
+  const board = document.querySelector('.board');
+  createItemBoard(board); // прорисовали поле
+  generateGoblin();
+}
+
+function createItemBoard(board) { // прорисовка поля
+  for (let i = 0; i < amountLines; i += 1) {
+    const tr = document.createElement('tr');
+    board.appendChild(tr);
+    for (let j = 0; j < amountСolumns; j += 1) {
+      const item = document.createElement('td');
+      tr.appendChild(item);
+      item.classList.add('board-item');
+    }
+  }
+  return board;
+}
+
+function clearAll() { // убирает класс гоблин там где он есть
+  const elements = document.getElementsByTagName('td');
+  for (let i = 0; i < elements.length; i += 1) {
+    elements[i].className = 'board-item';
+  }
+}
+
+function generateGoblin() {
+  const elements = document.getElementsByTagName('td');
+  let indNew = Math.floor(Math.random() * elements.length);
+  
+  elements[indNew].classList.add('goblin');
+ 
+}
+
+setInterval(() => {
+  clearAll(); 
+  generateGoblin();
+}, 2000);
